3fdc81b0456667c24dd3fa8cc07298870368b3f8
Jesica Sieiro <jesica.sieiro@gmail.com>
2022-07-19 21:44:04 -0300

    agregar navBar

---
+++ src/components/Container/Container.js
@@ -0,0 +1,14 @@
+const estilos={
+    padding:"40px",
+    margin:"auto",
+    color:"#f80"
+}
+
+function Container(){
+    return (
+        <div style={estilos}>
+            Hola Mundo!
+        </div>
+    );
+}
+export default Container;
