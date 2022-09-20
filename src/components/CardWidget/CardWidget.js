f705b08fd33b6b6509b10210587f7bf49f637ff5
Jesica Sieiro <jesica.sieiro@gmail.com>
2022-09-20 17:11:32 -0300

    mejora de cardWidget

--- src/components/CardWidget/CardWidget.js
+++ src/components/CardWidget/CardWidget.js
@@ -1,9 +1,11 @@
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import  "./CardWidget.css"
 import {CartContext} from "../../context/CartContext"
 import { useContext , useState } from "react"
 function CardWidget(){
-    const{cart}=useContext(CartContext);
+    const{handleTotalPrice,cart}=useContext(CartContext);
    
   const totalQuantityProds = cart.reduce((prev, next) => {
     return prev + next.quantity;
@@ -11,8 +13,12 @@ function CardWidget(){
 
     return(
         <div class="widgetContainer">
-            <p>{totalQuantityProds}</p>
-            <AddShoppingCartIcon color="primary" />
+           <Button  onClick={()=>handleTotalPrice(cart)} >
+            <Link to="/cart" className="btn-nav">
+              <p>{totalQuantityProds}</p>
+              <AddShoppingCartIcon color="primary" />
+            </Link>
+          </Button> 
         </div>
         
     )
